7b5ff39e1faed4b3e6956ae50708707a4df48cbf
ArcherSein <wkx1001@163.com>
2019-03-27 23:43:13 +0800

    update url
    update url

---
+++ README.md
@@ -0,0 +1,10 @@
+# web-crawler
+auto update url list.
+
+## Related 
+
+- [superagent](https://github.com/visionmedia/superagent)
+- [cheerio](https://github.com/cheeriojs/cheerio)
+- [crypto-js](https://github.com/brix/crypto-js)
+- [node-schedule](https://github.com/node-schedule/node-schedule)
+- [github-api](https://github.com/github-tools/github)
--- index.js
+++ index.js
@@ -1,62 +1,53 @@
-const fs = require('fs');
 const request = require('superagent');
 const cheerio = require('cheerio');
 const base64 = require('crypto-js/enc-base64');
 const utf8 = require('crypto-js/enc-utf8');
 const schedule = require('node-schedule');
-const { gitCommitPush } = require('git-commit-push-via-github-api');
+const GitHub = require('github-api');
 
 schedule.scheduleJob('3 0 * * *', getUrl);
 
 function getUrl() {
+  console.log('start to get url.');
   request
     .get('https://d.ishadowx.com/')
     .query({
       _: Date.now()
     })
-    .then(res => {
-      if (res.ok) {
-        try{
-          const $ = cheerio.load(res.text);
-          const btns = $('.v2 .copybtn');
-          const urls = Array.from(btns).map(btn => {
-            return $(btn).data('clipboardText');
-          });
-          const result = base64.stringify(utf8.parse(urls.join('')));
-          fs.writeFileSync('dist/url.txt', result);
-          pushToGithub();
-        }catch(e) {
-          console.log(e);
+    .then(
+      res => {
+        if (res.ok) {
+          try {
+            const $ = cheerio.load(res.text);
+            const btns = $('.v2 .copybtn');
+            const urls = Array.from(btns).map(btn => {
+              return $(btn).data('clipboardText');
+            });
+            const content = base64.stringify(utf8.parse(urls.join('')));
+            pushToGithub(content);
+          } catch (e) {
+            console.log(e);
+          }
         }
+      },
+      e => {
+        console.log(e);
       }
-    },(e) => {
-      console.log(e);
-    });
+    );
 }
 
-function pushToGithub() {
-  
-  process.on('unhandledRejection', console.dir);
-  if (!process.env.GITHUB_API_TOKEN) {
-    throw new Error('need GITHUB_API_TOKEN');
-  }
-  gitCommitPush({
-    owner: 'ArcherSein',
-    repo: 'web-crawler',
-    files: [
-      {
-        path: 'dist/url.txt',
-        content: fs.readFileSync(__dirname + '/dist/url.txt', 'utf-8')
-      }
-    ],
-    fullyQualifiedRef: 'heads/master',
-    forceUpdate: false, 
-    commitMessage: 'update at' + Date.now()
-  })
-    .then(res => {
-      console.log('success', res);
-    })
-    .catch(err => {
-      console.error(err);
-    });
+function pushToGithub(content) {
+  const gh = new GitHub({
+    token: process.env.GITHUB_API_TOKEN
+  });
+  let repo = gh.getRepo('ArcherSein', 'web-crawler');
+  repo.writeFile('master', 'dist/url.txt', content, 'update url list', function(
+    error,
+    result
+  ) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(result);
+  });
 }
